66e39e43574a2759a15b06b38035d7b19f3cf70d
Cbass <claveria.sebastian@gmail.com>
2022-09-24 13:14:47 -0300

    se envía correo. falta testing

--- app/src/main/java/cbassdagreat/github/centro_formacion_app/ui/DetalleFragment.kt
+++ app/src/main/java/cbassdagreat/github/centro_formacion_app/ui/DetalleFragment.kt
@@ -1,6 +1,7 @@
 package cbassdagreat.github.centro_formacion_app.ui
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Binder
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -31,6 +32,17 @@ class DetalleFragment : Fragment() {
             btnDetMail.setOnClickListener {
                 val nombre=tvDetNom.text
                 val intent = Intent(Intent.ACTION_SENDTO)
+                intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("admisión@centrofuturo.cl"))
+                intent.putExtra(Intent.EXTRA_SUBJECT,"Asunto: Solicito información sobre este curso ID $id")
+                intent.putExtra(Intent.EXTRA_TEXT,"Hola\n" +
+                        "Quisiera pedir información sobre este curso $nombre, me gustaría que me contactaran a este\n" +
+                        "correo o al siguiente número _________\n" +
+                        "Quedo atento.")
+                intent.setData(Uri.parse("mailto:"))
+                if(intent.resolveActivity(requireActivity().packageManager)!=null)
+                {
+                    startActivity(intent)
+                }
             }
         }
 
